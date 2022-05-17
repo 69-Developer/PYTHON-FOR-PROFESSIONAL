cc8cd954f320f48ae4d4452efacfe7f39bc441d6
Ezra <sumit.developer.69@gmail.com>
2022-05-17 22:03:47 +0530

    17-5-2022

---
+++ Python types and constructs/Integers.py
@@ -0,0 +1,63 @@
+myint = 1
+
+print(myint)
+
+myint = 12345678914567984161646164641616146464161
+
+print(myint)
+
+# get maximum of int values
+
+import sys
+
+print(sys.maxsize)
+
+# Simple operation with integers
+
+x = 5
+
+y = 10
+
+print(x+y)
+
+print(x-y)
+
+print(x*y)
+
+print(x//y)
+
+print(2**5)
+
+a = 1
+
+b= a
+
+print(b)
+
+# Short hand notation
+
+x = 10
+
+x *= 5
+
+print(x)
+
+"""
+WE ALSO HAVE THIS TYPES OF NOTATION FOR ADDITOIN += , SUBTRACTION -= , MULTIPLICATION *= , DIVISION /= , FLOOR DIVISION //= , MODULE DIVISION %= , POWER MODULE **=
+"""
+
+# conversation of number
+
+z = int(6.5)
+
+print(z)
+
+# converse string to integer
+
+abcd = '789'
+
+print(int(abcd))
+
+# This cannot work with this
+
+print(int('83.9'))
