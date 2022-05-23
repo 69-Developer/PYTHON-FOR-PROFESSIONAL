801122c4ba7cff5fcabfd64c426e75b6e355e9ec
Ezra <sumit.developer.69@gmail.com>
2022-05-23 08:46:23 +0530

    Variable naming

---
+++ Python types and constructs/Variable naming.py
@@ -0,0 +1,30 @@
+# Variable naming
+
+# 1. Variale names are case sensitive :- a and A are both different variable
+
+a = 5
+A = 10
+
+# print(a)
+# print(A)
+
+# 2. The first character in the python variable name must be letter. Once past the first letter you can use any combination of letters upper or
+# lower case, as well as numbers or the underscore character. A note here: Theere are special cases where a variable should begin with _
+
+Avariable = 5
+
+BVariable = 10
+
+# 566fse = 12 - This gives syntax error
+
+_country = "India"
+
+print(_country)
+
+# 3. Variables should not be reserved word like it range,while
+
+# 4. Single line in python should be length of 79
+
+x = None
+
+# None represents variable without value
